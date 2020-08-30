9dc51830747edce4a9c21371f0f4f3049d9c1d57
kate_gaw <ekaterinagaw@gmail.com>
2020-08-30 21:28:45 +0300

    build update

---
+++ build/precache-manifest.1694e1352f0ec737373c302fd334b8a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22343a33fb18eb6617feb2af27b6535",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26675460c82e59a4d9ef",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "22f46be0d841aae29966",
+    "url": "/static/js/2.a8a7a2de.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.a8a7a2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26675460c82e59a4d9ef",
+    "url": "/static/js/main.5e22e385.chunk.js"
+  },
+  {
+    "revision": "3012867a69f7b3dac8ab",
+    "url": "/static/js/runtime-main.43d01aab.js"
+  }
+]);
